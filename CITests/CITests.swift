996ac21b4bcfaf7d20d894de0426c3006d78856b
Laurent Noudohounsi <laurent.noudohounsi@gmail.com>
2019-12-19 14:05:29 +0100

    Add tests

--- CITests/CITests.swift
+++ CITests/CITests.swift
@@ -11,24 +11,26 @@ import XCTest
 
 class CITests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  //----------------------------------------------------------------------------
+  // MARK: - Initialization
+  //----------------------------------------------------------------------------
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func setUp() {
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  override func tearDown() {
+  }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  //----------------------------------------------------------------------------
+  // MARK: - Test
+  //----------------------------------------------------------------------------
+
+  func testSuccess() {
+    XCTAssert(true, "Should not fail.")
+  }
+
+  func testFailure() {
+    XCTAssert(false, "Is supposed to fail.")
+  }
 
 }
