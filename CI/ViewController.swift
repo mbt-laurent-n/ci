cd8220b25569c6d9bf37f1151153ecf19b76a06a
Laurent Noudohounsi <laurent.noudohounsi@gmail.com>
2019-12-19 14:02:53 +0100

    Add iOS project

---
+++ CI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI
+//
+//  Created by Laurent Noudohounsi on 19/12/2019.
+//  Copyright © 2019 MyBrainTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
